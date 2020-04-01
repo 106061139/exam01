6576439743d0de5f58ccddffc5ea4a1fced773a4
106061139 <s1061061139@m106.nthu.edu.tw>
2020-04-01 16:56:41 +0800

    Add main.cpp

--- exam01/main.cpp
+++ exam01/main.cpp
@@ -1,13 +1,13 @@
 #include "mbed.h"
+#include "uLCD_4DGL.h"
 
 
+uLCD_4DGL uLCD(D1, D0, D2);
 AnalogOut Aout(DAC0_OUT);
 
 
 int main(){
-
-float i;
-
+    uLCD.printf("\n106061139\n");
   while(1){
       Aout= 1;
       wait(1);
