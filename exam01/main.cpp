09167afe63ac805f2d14c5ddd1595f23ae58f2a0
106061139 <s1061061139@m106.nthu.edu.tw>
2020-04-01 17:45:32 +0800

    Add main.cpp

--- exam01/main.cpp
+++ exam01/main.cpp
@@ -6,29 +6,32 @@ uLCD_4DGL uLCD(D1, D0, D2);
 AnalogOut Aout(DAC0_OUT);
 PwmOut PWM1(D6);
 Serial pc( USBTX, USBRX );
+DigitalOut D(D0);
+float data[128];
 
 int main(){
     float p;
+   
+ //   while(1){
     uLCD.printf("\n106061139\n");
-    while(1){
     PWM1.period(0.001);
-    for(float i=0; i<1;i+=0.1){   
-    PWM1 = i;
-    p=PWM1;
-    wait(0.1);
-    pc.printf("%1.3f\r\n",p);  
+    for(float i=0; i<1;i+=0.1){       
+        PWM1 = i;
+        p=PWM1;
+        wait(0.1);
+        pc.printf("%1.3f\r\n",p);  
     }
     for(float i=1; i<0;i-=0.1){   
-    PWM1 = i;
-    p=PWM1;
-    wait(0.1);
-    pc.printf("%1.3f\r\n",p);  
+        PWM1 = i;
+        p=PWM1;
+        wait(0.1);
+        pc.printf("%1.3f\r\n",p);  
     }
   
  //     Aout= 1;
  //     wait(1);
  //     Aout= 0;
  //     wait(1);
-  }
+//  }
 
 }
